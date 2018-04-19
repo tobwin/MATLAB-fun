211e0f722062a914f421d43c123050d48ec82398
Tobias Winner <t.winner@donders.ru.nl>
2018-04-19 14:07:18 +0200

    Update randnm.m

--- randnm.m
+++ randnm.m
@@ -1,7 +1,8 @@
 function X = randnm(M, V, U, vargargin)
 %randnm Random matrix
 %   randnm(M, V, U) returns a random normal matrix ~NM(X|M,V,U)
-%   randnm(M, V, U, 'chol') accepts Cholesky decomposed U and V
+%   randnm(M, V, U, 'chol') accepts lower-triangular U and V and samples
+%   from ~NM(X|M,VV',UU')
 %
 % Tobias Winner, 19.04.2018
 % email: winner.tobias@gmail.com
@@ -9,11 +10,14 @@ function X = randnm(M, V, U, vargargin)
 n = size(V, 1);
 p = size(U, 1);
 if all(all(M==0))
-    M = zeros(n, p); 
+    M = zeros(n, p);
 end
-if ismember('chol',varargin)
-    vecX = kron(V, U) * randn(n * p, 1);
+if nargin > 3
+    if ismember('chol',varargin)
+        vecX = kron(V, U) * randn(n * p, 1);
+    end
 else
-    vecX = chol(kron(V, U)) * randn(n * p, 1);
+    vecX = chol(kron(V, U),'lower') * randn(n * p, 1);
 end
 X = reshape(vecX, n, p) + M;
+end
