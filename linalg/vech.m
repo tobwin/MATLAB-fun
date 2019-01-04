0f60032f8d691994205536cd9abbd72e8491fa7b
Tobias Winner <t.winner@donders.ru.nl>
2019-01-04 12:54:22 +0100

    Create vech.m

---
+++ linalg/vech.m
@@ -0,0 +1,8 @@
+function x = vech(X)
+% vech(X) returns the vectorization x = [X(1,1) X(1,2) ... X(1,n) X(2,2) X(2,1) ... X(n,n)  ]' for symmetric matrices X.
+
+assert(all(all(X==X')), 'X must be symmetric.')
+
+x = X(triu(true(size(X))));
+
+end
