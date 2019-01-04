f5833a97fbd51d4b4a5795f456df2bf7ec06b779
Tobias Winner <t.winner@donders.ru.nl>
2019-01-04 12:56:25 +0100

    Update vech.m

--- linalg/vech.m
+++ linalg/vech.m
@@ -1,5 +1,6 @@
 function x = vech(X)
-% vech(X) returns the vectorization x = [X(1,1) X(1,2) ... X(1,n) X(2,2) X(2,1) ... X(n,n)  ]' for symmetric matrices X.
+% vech(X) returns the vectorization x = [X(1,1) X(1,2) ... X(1,n) X(2,2) X(2,1) ... X(n,n)]' 
+%         for symmetric matrices X.
 
 assert(all(all(X==X')), 'X must be symmetric.')
 
